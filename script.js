ee6c5f8896538d72e9a66168c878ecc27f84e13d
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-28 22:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	if(text.length === 0) return "";
+	let ans  = "";
+	let i=0; 
+	while((i < text.length) && (text.charAt(i) === " ")) i++;
+
+	if(i < text.length) ans = text.charAt(i);
+	return ans;
 }
 
-// Do not change the code below
+// Do not change the code below    
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
